a65d255d04fa9ed04bd0645d1fbb6da035b3b5d3
ND-Irawan <indracoy01@gmail.com>
2025-04-10 18:43:44 +0700

    Revert "sched: Introduce task-rotation feature"
    This reverts commit 9c4d7f04ad164f4270239d776ae36cfd82574d8d.

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1509,14 +1509,6 @@ struct task_struct {
 	/* Used by LSM modules for access restriction: */
 	void				*security;
 #endif
-<<<<<<< HEAD
-=======
-
-#ifdef CONFIG_SPRD_ROTATION_TASK
-	u64 last_enqueue_ts;
-#endif
-
->>>>>>> 33f56798f7d6b (sched: Introduce task-rotation feature)
 	/* task is frozen/stopped (used by the cgroup freezer) */
 	ANDROID_KABI_USE(1, unsigned frozen:1);
 
