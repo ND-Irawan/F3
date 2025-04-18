7777e4d0f7f7df6641802b5f89bc67d8a9c4a8ee
ND-Irawan <indracoy01@gmail.com>
2025-04-18 23:48:28 +0700

    mm: Tweak memory pressure

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -323,8 +323,8 @@ compound_page_dtor * const compound_page_dtors[] = {
  */
 int min_free_kbytes = 1024;
 int user_min_free_kbytes = -1;
-int watermark_boost_factor __read_mostly;
-int watermark_scale_factor = 10;
+int watermark_boost_factor __read_mostly = 15000;
+int watermark_scale_factor = 50;
 
 /*
  * Extra memory for the system to try freeing. Used to temporarily
