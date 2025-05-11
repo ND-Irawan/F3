ba014395fab8ad8f83f1cd1c1cf16f5b9572e87b
ND-Irawan <indracoy01@gmail.com>
2025-05-11 20:33:07 +0700

    Revert "sched: Introduce Per-Cluster DVFS Headroom"
    This reverts commit c6e732d2a4106c136936ae31326fdc1c4f184262.

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -437,36 +437,6 @@ unsigned long schedutil_cpu_util(int cpu, unsigned long util_cfs,
 	util = scale_irq_capacity(util, irq, max);
 	util += irq;
 
-	return min(scale, util);
-}
-
-static __always_inline
-unsigned long apply_dvfs_headroom(int cpu, unsigned long util, unsigned long max_cap)
-{
-	unsigned long headroom;
-
-	if (!util || util >= max_cap || cpumask_test_cpu(cpu, cpu_prime_mask))
-		return util;
-
-	if (cpumask_test_cpu(cpu, cpu_lp_mask)) {
-		headroom = util + (util >> 1);
-	} else {
-		headroom = util + (util >> 2);
-	}
-
-	return headroom;
-}
-
-unsigned long sugov_effective_cpu_perf(int cpu, unsigned long actual,
-				 unsigned long min,
-				 unsigned long max)
-{
-	/* Add dvfs headroom to actual utilization */
-	actual = apply_dvfs_headroom(cpu, actual, max);
-	/* Actually we don't need to target the max performance */
-	if (actual < max)
-		max = actual;
-
 	/*
 	 * Bandwidth required by DEADLINE must always be granted while, for
 	 * FAIR and RT, we use blocked utilization of IDLE CPUs as a mechanism
