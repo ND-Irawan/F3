63a9b20e7e348f2af8f3cecc5e9dcfff210a31b8
ND-Irawan <indracoy01@gmail.com>
2025-05-14 23:08:31 +0700

    Revert "cpufreq schedutil"
    This reverts commit 17f949e2d7faf7fb030433a170ad87dd2d401515.

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -1013,6 +1013,7 @@ static struct attribute *sugov_attributes[] = {
 	NULL
 };
 
+static void sugov_tunables_free(struct kobject *kobj)
 {
 	struct gov_attr_set *attr_set = container_of(kobj, struct gov_attr_set, kobj);
 
@@ -1224,6 +1225,7 @@ static void sugov_exit(struct cpufreq_policy *policy)
 	count = gov_attr_set_put(&tunables->attr_set, &sg_policy->tunables_hook);
 	policy->governor_data = NULL;
 	if (!count) {
+		sugov_tunables_save(policy, tunables);
 		sugov_clear_global_tunables();
 	}
 
