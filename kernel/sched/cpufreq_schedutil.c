17f949e2d7faf7fb030433a170ad87dd2d401515
ND-Irawan <indracoy01@gmail.com>
2025-05-14 23:04:39 +0700

    cpufreq schedutil

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -1013,7 +1013,6 @@ static struct attribute *sugov_attributes[] = {
 	NULL
 };
 
-static void sugov_tunables_free(struct kobject *kobj)
 {
 	struct gov_attr_set *attr_set = container_of(kobj, struct gov_attr_set, kobj);
 
@@ -1225,7 +1224,6 @@ static void sugov_exit(struct cpufreq_policy *policy)
 	count = gov_attr_set_put(&tunables->attr_set, &sg_policy->tunables_hook);
 	policy->governor_data = NULL;
 	if (!count) {
-		sugov_tunables_save(policy, tunables);
 		sugov_clear_global_tunables();
 	}
 
